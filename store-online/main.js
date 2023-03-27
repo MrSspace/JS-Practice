db3ceb4aad11120643e0b61140b127bada5dcd61
Sergio Arango <estudiospersonales01@gmail.com>
2023-03-27 16:02:27 -0500

    added mobile menu

--- store-online/main.js
+++ store-online/main.js
@@ -1,8 +1,16 @@
 const emailNavBar = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const mobileIconMenu = document.querySelector('.mobile-icon-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
+
 emailNavBar.addEventListener('click', toogleDesktopMenu);
+mobileIconMenu.addEventListener('click', toogleMobileMenu);
