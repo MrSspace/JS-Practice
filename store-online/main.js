1908f282bebdcd4de61ea66b5f56c8b2fe79fec9
Sergio Arango <estudiospersonales01@gmail.com>
2023-04-05 00:31:05 -0500

    Added open and closed functionalities for productDetails

--- store-online/main.js
+++ store-online/main.js
@@ -5,22 +5,35 @@ const mobileIconMenu = document.querySelector('.mobile-icon-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 const menuCarIcon = document.querySelector('.navbar-shopping-cart');
-const shoppingCartContainer = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppin-cart-container');
 
 const cardsContainer = document.querySelector('.cards-container');
 
+const productDetailContainer = document.querySelector('#product-detail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+
 function toogleDesktopMenu(){
     const isCarAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
     if(!isCarAsideClosed){
         shoppingCartContainer.classList.add('inactive');
+    }
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu(){
     const isCarAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
     if(!isCarAsideClosed){
         shoppingCartContainer.classList.add('inactive');
+    }
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -28,18 +41,36 @@ function toogleMobileMenu(){
 function toogleCarAside(){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
     if(!isDesktopMenuClosed){
         desktopMenu.classList.add('inactive');
     }
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
     shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProducDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
 }
 
 emailNavBar.addEventListener('click', toogleDesktopMenu);
 mobileIconMenu.addEventListener('click', toogleMobileMenu);
 menuCarIcon.addEventListener('click', toogleCarAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 const products = [];
 products.push(
@@ -71,6 +102,7 @@ function renderProducts(arr){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProducDetailAside);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
